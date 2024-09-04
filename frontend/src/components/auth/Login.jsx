df24c3055eba79a53c973478975aaac7bd51d6e8
MonishwaranB <monishwaran1504@gmail.com>
2024-09-04 12:35:07 +0530

    Login finished

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -5,14 +5,19 @@ import './Login.css'
 const Login = () => {
   return (
     <div className='login'>
-      <div className='login-container'>
-          <img className='img' src={logo} alt="logo"/> 
-          <h1>Jira</h1>
-          <p>Login to to continue</p>
-          <input type="emai" required/> <br />
-          <input type="password" required/> <br />
-          <button>Login</button>
-          <p><span>Does not Have an account ?</span> <span>Sign up here!</span></p>
+       <div className='login-container'>
+          <div className='logo-con'>
+            <img className='img' src={logo} alt="logo"/> <span className='name-logo'>Jira</span>
+            <p className='font'>Login to to continue</p>
+              <div className='ip-box font'>
+                <input  className='input i' type="emai" placeholder='Enter your Email-id' required/> <br />
+                <input className='input i' type="password" required placeholder='Enter Password'/> <br />
+                <button className='input'>Login</button>
+              </div>
+            </div>
+          <div className='end'>
+            <p className='font'><span>Does not Have an account ?</span> <span className='sign-up'>Sign up here!</span></p>
+          </div>
        </div>
     </div>
   )
