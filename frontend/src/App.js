d8dd7399e61cdaa98ea3cb6e5d3ed9a77e5e2dbf
MonishwaranB <monishwaran1504@gmail.com>
2024-09-04 13:07:19 +0530

    Routes initialized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Login from './components/auth/Login';
-import Body from './components/LandingPage/Body';
-import Header from './components/LandingPage/Header';
+import Signup from './components/auth/Signup';
+import Landing from './components/LandingPage/Landing';
 
 function App() {
   return (
-    <div className="App">
-       <Login/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Landing/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<Signup/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
