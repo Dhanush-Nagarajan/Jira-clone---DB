d0846bf69d69bfad886d3357d435352dde6d3e30
MonishwaranB <monishwaran1504@gmail.com>
2024-09-09 20:10:54 +0530

    project page side bar ui initiallized

--- frontend/src/components/HomePage/Home.jsx
+++ frontend/src/components/HomePage/Home.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Navbar from './Navbar'
 import Body from './Body'
 import CreateProject from '../CreateProject/CreateProject'
+import ActiveProject from '../ActiveProject/ActiveProject'
 
 const Home = () => {
   return (
     <>
-    <CreateProject/>
+    <ActiveProject/>
     </>
   )
 }
