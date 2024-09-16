b87c4f96225ef5da4812bbe83275556e4768e035
DhanushAKian <dnush1942@gmail.com>
2024-09-16 14:39:29 +0530

    added getProjectforUsers api

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import protectRoute1 from '../middleware/authenticateUserProject.js';
 import { getUsersForProject } from '../controllers/userController.js';
+import { GetProjectForUser } from '../controllers/getProjectController.js';
 
 const router = express.Router();
 router.get("/getusers",protectRoute1,getUsersForProject);
+router.get("/getprojects",protectRoute1,GetProjectForUser);
 
 export default router;
