c09ea170eaddc7c0a80e9ddbadc7e184c80fcdb3
DhanushAKian <dnush1942@gmail.com>
2024-09-16 14:06:58 +0530

    Token verification must be done using Headers, not cookies

--- Backend/routes/addMemberRoutes.js
+++ Backend/routes/addMemberRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import protectRoute from '../middleware/authenticateUser.js';
+import protectRoute1 from '../middleware/authenticateUserProject.js';
 import { addMember } from '../controllers/addMemberController.js';
 
 const router = express.Router();
 
-router.post("/add/:projectId/:newUserId", protectRoute, addMember);
+router.post("/add/:projectId/:newUserId", protectRoute1, addMember);
 
 export default router;
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import protectRoute from '../middleware/authenticateUser.js';
+import protectRoute1 from '../middleware/authenticateUserProject.js';
 import { getUsersForProject } from '../controllers/userController.js';
 
 const router = express.Router();
-router.get("/getusers",protectRoute,getUsersForProject);
+router.get("/getusers",protectRoute1,getUsersForProject);
 
 export default router;
