d9e1a55c9953231287323ddd49fc91da2d03192a
Shaikabdu18 <shaikabdu49@gmail.com>
2024-09-18 15:05:37 +0530

    Delete Project Api created

--- Backend/routes/projectRoutes.js
+++ Backend/routes/projectRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import {createProject} from '../controllers/projectController.js';
 import protectRoute1 from '../middleware/authenticateUserProject.js';
+import {deleteProject} from "../controllers/deleteProjectController.js";
+
 
 const router = express.Router();
 
 router.post("/createproject",protectRoute1,createProject);
+router.delete("/deleteproject/:projectId",protectRoute1,deleteProject)
 
 export default router;
