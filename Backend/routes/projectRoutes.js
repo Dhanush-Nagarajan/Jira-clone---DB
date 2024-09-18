3d3738d99c02c670b7c6a9cb81d2db45d2ac768f
DhanushAKian <dnush1942@gmail.com>
2024-09-18 15:15:24 +0530

    fixed error

--- Backend/routes/projectRoutes.js
+++ Backend/routes/projectRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import {createProject} from '../controllers/projectController.js';
-import protectRoute1 from '../middleware/authenticateUserProject.js';
+import protectRoute from '../middleware/authenticateUser.js';
 import {deleteProject} from "../controllers/deleteProjectController.js";
 
 
 const router = express.Router();
 
-router.post("/createproject",protectRoute1,createProject);
-router.delete("/deleteproject/:projectId",protectRoute1,deleteProject)
+router.post("/createproject",protectRoute,createProject);
+router.delete("/deleteproject/:projectId",protectRoute,deleteProject)
 
 export default router;
