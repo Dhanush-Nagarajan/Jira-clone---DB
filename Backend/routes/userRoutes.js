50fa023c960670d56d09ecf54ff13b98cf41e725
DhanushAKian <dnush1942@gmail.com>
2024-09-19 11:50:04 +0530

    get participants function added

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -3,11 +3,12 @@ import protectRoute from '../middleware/authenticateUser.js';
 import { getUsersForProject } from '../controllers/userController.js';
 import { GetProjectForUser } from '../controllers/getProjectController.js';
 import { getLead } from '../controllers/getLeadController.js';
+import { getParticipants } from '../controllers/getParticpantsController.js';
 
 const router = express.Router();
 router.get("/getusers/:ProjectID",protectRoute,getUsersForProject);
 router.get("/getprojects",protectRoute,GetProjectForUser);
 router.get("/getlead/:userID",protectRoute,getLead);
-
+router.get("/getparticipants/:ProjectID",protectRoute,getParticipants);
 
 export default router;
