845186b0d460705b077ea355e2f7a2ee9318ea95
Shaikabdu18 <shaikabdu49@gmail.com>
2024-09-25 13:53:53 +0530

    fix:issue

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -7,9 +7,8 @@ import generateTokenAndSetCookie from "../utils/generateToken.js";
 export const signup = async (req, res) => {
     try {
       const { fullName, email, password, confirmPassword } = req.body;
-      const { token } = req.query; // For invited users
+      const { token } = req.query; 
   
-    //   console.log("Received token: ", token);
   
       const lowerCaseEmail = email.toLowerCase();
   
@@ -17,13 +16,11 @@ export const signup = async (req, res) => {
         return res.status(400).json({ error: "Passwords do not match!" });
       }
   
-      // Check if the user already exists
       const user = await User.findOne({ email: lowerCaseEmail });
       if (user) {
         return res.status(400).json({ error: "Email already exists!" });
       }
   
-      // If token is provided, validate the invitation
       let invitation = null;
       if (token) {
         invitation = await Invitation.findOne({ token });
@@ -31,13 +28,11 @@ export const signup = async (req, res) => {
           return res.status(400).json({ error: "Invalid or expired invitation token." });
         }
   
-        // Check if the invitation's email matches the registration email
         if (invitation.email.toLowerCase() !== lowerCaseEmail) {
           return res.status(400).json({ error: "Email does not match the invitation email." });
         }
       }
   
-      // Hash the password and create the user
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(password, salt);
   
@@ -50,17 +45,14 @@ export const signup = async (req, res) => {
       await newUser.save();
   
       if (newUser) {
-        // Generate token and set it in the cookie
         generateTokenAndSetCookie(newUser._id, res);
   
-        // If user signed up through an invitation, add them to the project
         if (invitation) {
           const project = await Project.findById(invitation.projectId);
           if (project) {
             project.participants.push(newUser._id);
             await project.save();
   
-            // Mark invitation as 'Accepted'
             invitation.status = 'Accepted';
             await invitation.save();
           }
